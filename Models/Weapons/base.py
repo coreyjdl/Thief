c797097fdf204e3bd6e96eff5248bfac8cf37ecc
coreyjdl <coreyjdl@gmail.com>
2021-01-15 15:35:41 -0600

    added empty readme

---
+++ Models/Weapons/base.py
@@ -0,0 +1,18 @@
+from abc import ABC
+
+class Weapon:
+
+    def __init__(self):
+        ...
+    
+    def get_type(self):
+        ...
+    
+    def get_weight(self):
+        ...
+    
+    def get_ammo(self):
+        ...
+    
+    def set_ammo(self):
+        ...
